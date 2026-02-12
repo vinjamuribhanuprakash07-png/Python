6329d45a5594038848381114f04d557da1512752
Bhanu Prakash Vinjamuri <bhanuprakashvinjamuri@Mac.communityfibre.co.uk>
2026-02-12 14:13:24 +0000

    Add First_one.py

---
+++ First_one.py
@@ -0,0 +1,114 @@
+# print('hello world');
+
+a=10;
+b=20;
+c=a+b;
+print(c);
+print('Sum is:', c);
+
+# x=5
+# if x<10:
+#     print('samllest')
+# if x>20:
+#     print('greaster')
+# print('finesih')
+
+# n=5
+# while n>0:
+#     print(n)
+#     n=n-1
+# print('blastoff')
+
+# x=10
+# print(x)
+# print(type(x))
+
+# x='srini'
+# print(x)
+# print(type(x))
+
+# x=15
+# y=20
+# x=90
+# print(x)
+
+# name =('how are you')
+# print('srini',name)
+
+# inp=input('europe floor?')
+# ami=int(inp)+1
+# print('us floor' ,ami)
+
+# x=2
+# if x<2:
+#     print('Below 2')
+# elif x>=2:
+#     print('two or more')
+# else:
+#     print('something else')
+
+# x = 0
+# if x < 2 :
+#     print('Small')
+# elif x < 10 :
+#     print('Medium')
+# else :
+#     print('LARGE')
+# print('All done')
+
+# if x < 2 :
+#     print('Below 2')
+# elif x >= 2 :
+#      print('Two or more')
+# else :
+#     print('Something else')
+
+# astr = 'Hello Bob'
+# istr = 0
+# try:
+#     istr = int(astr)
+# except:
+#     istr = -1
+
+# def things():
+#     print('hello')
+#     print('fun')
+# things()
+# print('zip')
+# things()
+
+# x = 'banana'
+# y = max(x)
+# z = y * 2
+# print(z)
+
+# def func(x) :
+#     print(x)
+# func(10)
+# func(20)
+
+# def stuff():
+#     print('Hello')
+#     return
+#     print('World')
+
+# stuff()
+
+# def addtwo(a, b):
+#     added = a + b
+#     return a
+
+# x = addtwo(2, 7)
+# print(x)
+
+# tot = 0 
+# for i in [5, 4, 3, 2, 1] :
+#     tot = tot + 1
+# print(tot)
+
+# n = 0
+# while n > 0 :
+#     print('Lather')
+#     print('Rinse')
+# print('Dry off!')
+
